1c1c0f191b016c1b1f438041feb10845a51b40bf
José Jaime <rpjosejaime@gmail.com>
2024-04-08 15:44:38 -0600

    archivo js

--- resources/views/escolares/planes-estudio.blade.php
+++ resources/views/escolares/planes-estudio.blade.php
@@ -15,10 +15,39 @@
                     <tr>
                         <td>{{ $item->clave_plan_estudio }}</td>
                         <td>{{ $item->carrera }}</td>
+                        <td>
+                            <div class="buttons">
+                                <button class="button is-warning">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                </button>
+                                <button class="button is-danger">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </button>
+                            </div>
+                        </td>
                     <tr>
                 @endforeach
             </tbody>
         </table>
+
+        <button class="js-modal-trigger" data-target="modal-js-example">
+            Open JS example modal
+        </button>
+
+
+        <div id="modal-js-example" class="modal">
+            <div class="modal-background"></div>
+
+            <div class="modal-content">
+                <div class="box">
+                    <p>Modal JS example</p>
+                    <!-- Your content -->
+                </div>
+            </div>
+
+            <button class="modal-close is-large" aria-label="close"></button>
+        </div>
+
 
     </div>
 @endsection
